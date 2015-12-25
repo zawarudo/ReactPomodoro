e6e69ebc318a99a9691439fb9b4205d23d7074f7
Mihai <mbadescu51@gmail.com>
2015-12-25 09:03:57 -0600

    Added alarm sound on timer finished

--- src/pomodoro.jsx
+++ src/pomodoro.jsx
@@ -22,16 +22,26 @@ module.exports = React.createClass({
 
 		if(time < 0) { time = 0; };
 		
-		if(time === 0) { newState.paused = true; };
+		if(time === 0) {
+			newState.paused = true;
+			this.alarmPlay();
+		};
 
 		newState.time = time;
 
 		this.setState(newState);
 	},
+
+	alarmPlay: function() {
+		var playerElement = this.refs.alarm.getDOMNode();
+		playerElement.play();
+	},
+
 	render: function() {
 		return <div className='text-center'>
 			<Timer currTime={this.state.time}/>
 			<Controls handleBtnClick={this.controlsBtnClicked} paused={this.state.paused}/>
+			<audio ref='alarm' src='../audio/alarm.mp3'></audio>
 		</div>
 	},
 
